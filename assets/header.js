2663c5ad4ddedd77459cc0f9ad529c365a31766a
Andrew Aguirre <andrewa96@ymail.com>
2023-04-12 13:59:55 -0700

    add footer svgs, add header hover menus

--- assets/header.js
+++ assets/header.js
@@ -1,3 +1,26 @@
-// document.querySelector('[aria-controls="radix-3"]').addEventListener('click', (e) => {
-//   document.getElementById('radix-3').toggleAttribute('hidden')
-// })
+document.querySelector('[aria-controls="radix-3"]').addEventListener('click', (e) => {
+  document.getElementById('radix-3').toggleAttribute('hidden')
+})
+
+
+// Handle menu hover
+const explMerchEl = document.getElementById('radix-4-trigger-radix-0');
+const explMerchMenu = document.getElementById('headerHoverMenuDesktop1');
+let menuTimeout;
+
+const explMerchMenuSetStyle = (display = 'none') => {
+  if (display === 'none' && explMerchMenu.matches(":hover")) {
+      menuTimeout = setTimeout(explMerchMenuSetStyle, 500);
+  } else {
+    explMerchMenu.style.display = display
+  }
+}
+
+explMerchEl.addEventListener('mouseenter', (e) => {
+  clearTimeout(menuTimeout);
+  explMerchMenuSetStyle('flex');
+})
+
+explMerchEl.addEventListener('mouseleave', (e) => {
+  menuTimeout = setTimeout(explMerchMenuSetStyle, 500)
+})
